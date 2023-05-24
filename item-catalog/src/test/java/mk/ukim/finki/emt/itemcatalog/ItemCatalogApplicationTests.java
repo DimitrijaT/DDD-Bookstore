b7b028cebf964337fc801a210df894b50b92e2ae
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-05-25 01:36:20 +0200

    Project setup

---
+++ item-catalog/src/test/java/mk/ukim/finki/emt/itemcatalog/ItemCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.itemcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
