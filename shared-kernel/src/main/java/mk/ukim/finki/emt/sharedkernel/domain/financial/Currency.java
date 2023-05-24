65b04d073ea4fc36f33aba104d41211e1bd54e38
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-05-25 01:54:57 +0200

    Package setup and shared-kernel implementation

---
+++ shared-kernel/src/main/java/mk/ukim/finki/emt/sharedkernel/domain/financial/Currency.java
@@ -0,0 +1,14 @@
+package mk.ukim.finki.emt.sharedkernel.domain.financial;
+
+public enum Currency {
+    EUR, USD, MKD,
+    AUD, CAD, CHF,
+    CNY, JPY, NZD,
+    ZAR, BGN, CZK,
+    DKK, EEK, HUF,
+    LTL, LVL, NOK,
+    PLN, RON, RUB,
+    SEK, TRY, HRK,
+    KRW, MXN, MYR,
+    PHP, SGD, THB
+}
